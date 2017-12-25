b0fee855cdfbec44917e5171408dab0adbeac91b
jasmaa <jasonmaa3955@gmail.com>
2017-12-25 12:51:34 -0500

    Room joining works

--- protobowl.py
+++ protobowl.py
@@ -8,17 +8,17 @@ def build_join(room):
     data['name'] = 'join'
 
     args = {}
-    args['cookie'] = 'PB4CLsdc5gkhwvnqt8ugkplo3t4e9qeqco679v2s1'
+    args['cookie'] = 'asdf'
     args['auth'] = None
     args['question_type'] = 'qb'
+    args['room_name'] = room
     args['muwave'] = False
     args['agent'] = 'M4/Web'
     args['agent_version'] = 'Sat Sep 02 2017 11:33:43 GMT-0700 (PDT)'
     args['version'] = 8
-
     data['args'] = args
 
-    return '5::: ' + json.dumps(data)
+    return '5:::' + json.dumps(data)
 
 # connect
 server = "ocean.protobowl.com:443/socket.io/1/websocket/"
@@ -29,6 +29,7 @@ print("Socket = " + socketString)
 
 ws = websocket.WebSocket()
 ws.connect("ws://"+server+socketString)
-while True:
-    ws.send(build_join("direac-arousing-chicken"))
-    print(ws.recv())
+
+ws.send(build_join("actor-touching-chicken"))
+
+print(ws.recv())
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Python ProtoBot
 
-An attempt at writing a bot for Protobowl in Python 3
+An attempt at writing a bot for Protobowl in Python 3 with heavy reference to [ProtoBot](https://github.com/bobacadodl/ProtoBot)
