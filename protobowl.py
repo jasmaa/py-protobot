f6f7389657f025a44a23bbc188ec775694c3410d
jasmaa <jasonmaa3955@gmail.com>
2017-12-25 17:15:52 -0500

    Refactor code

--- protobowl.py
+++ protobowl.py
@@ -1,56 +1,70 @@
-"""Commands for interacting with PB"""
+""" PB api """
 
 import json
+import requests
+import websocket
+import pprint as pp
+import threading
 
-def join(room):
+class ProtoBowl:
 
-    data = {}
-    data['name'] = 'join'
+    ws = websocket.WebSocket()
+    server = 'ocean.protobowl.com:443/socket.io/1/websocket/'
 
-    args = {}
-    args['cookie'] = 'asdf'
-    args['auth'] = None
-    args['question_type'] = 'qb'
-    args['room_name'] = room
-    args['muwave'] = False
-    args['agent'] = 'M4/Web'
-    args['agent_version'] = 'Sat Sep 02 2017 11:33:43 GMT-0700 (PDT)'
-    args['version'] = 8
-    data['args'] = args
+    def __init__(self, room):
+        # STATE VARS
+        self.room_name = room
+        self.name = None
+        self.qid = ''
+        self.ans = ''
 
-    return '5:::' + json.dumps(data)
-
-def buzz(qid):
-    data = {}
-
-    data['name'] = 'buzz'
-    args = [1]
-    args[0] = qid
+    def update(self):
+        while True:
+            try:
+                raw_data = self.ws.recv().split(':::')
+                if raw_data[0] == '5':
+                    self.qid = json.loads(raw_data[1])['args'][0]['qid']
+                    self.ans = json.loads(raw_data[1])['args'][0]['answer']
+            except:
+                None
     
-    data['args'] = args
+    def connect(self):
+        r = requests.get('http://'+self.server)
+        socketString = r.text.split(':')[0]
+        print('Socket = ' + socketString)
 
-    return "5:23+::" + json.dumps(data)
+        self.ws.connect('ws://'+self.server+socketString)
 
-def guess(guess):
-    data = {}
-    data['name'] = 'guess'
+        self.join('batman', self.room_name)
 
-    args = [2]
-    arg_data = {}
-    arg_data['text'] = guess
-    arg_data['done'] = True
+        t = threading.Thread(target=self.update)
+        t.start()
+    
+    def answer(self, guess):
+        self.buzz(self.qid)
+        self.guess(guess, True)
 
-    args[0] = arg_data
-    data['args'] = args
+    """Raw commands for interacting with PB"""
+    def join(self, cookie, room_name):
+        self.ws.send('5:::{"name":"join","args":[{"cookie":"' + cookie + '","auth":null,"question_type":"qb","room_name":"' + room_name + '","muwave":false,"agent":"M4/Web","agent_version":"Sat Sep 02 2017 11:33:43 GMT-0700 (PDT)","version":8}]}')
 
-    return '5:::' + json.dumps(data)
+    def set_name(self, name):
+        self.ws.send('5:::{"name":"set_name","args":["' + name + '",null]}')
 
-def next_question():
-    data = {}
+    def buzz(self, qid):
+        self.ws.send('5:23+::{"name":"buzz","args":["' + qid + '"]}')
 
-    data['name'] = 'next'
-    args = [2]
+    def guess(self, guess, done):
+        self.ws.send('5:::{"name":"guess","args":[{"text":"' + guess + '","done":true},null]}')
 
-    data['args'] = args
+    def next(self):
+        self.ws.send('5:::{"name":"next","args":[null,null]}')
 
-    return "5:::" + json.dumps(data)
+    def pause(self):
+        self.ws.send('5:::{"name":"pause","args":[null,null]}')
+
+    def unpause(self):
+        self.ws.send('5:::{"name":"unpause","args":[null,null]}')
+
+    def ping(self):
+        self.ws.send('2::')
